66c123d628184237c41c3b766344f3d2785515a8
Rhuturaj-Rogaye <rhuturajrogaye@gmail.com>
2025-11-19 01:26:51 +0530

    Delete first.py

--- first.py
+++
@@ -1,4 +0,0 @@
-print("hello world")
-# output of programis is what the program shows after running
-# file extension is a small ending part of file's name that tells computer what kind of file it is.
-# A python module is like a toolbox filled with special tools(codes).
