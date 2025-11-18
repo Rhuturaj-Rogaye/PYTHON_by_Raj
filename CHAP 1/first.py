bf2f85c5448ba113577077b1b4a2973de3f888cb
Rhuturaj-Rogaye <rhuturajrogaye@gmail.com>
2025-11-19 01:22:14 +0530

    Add files via upload

---
+++ CHAP 1/first.py
@@ -0,0 +1,4 @@
+print("hello world")
+# output of programis is what the program shows after running
+# file extension is a small ending part of file's name that tells computer what kind of file it is.
+# A python module is like a toolbox filled with special tools(codes).
